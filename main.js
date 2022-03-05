995625d824470d3252852b0a1ca5578d5d706bfb
git-kamesh <1234.kamesh@gmail.com>
2022-03-05 17:47:51 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+function Button(domQuery) {
+  let className = `btn-${Math.random()}`;
+  let state = {
+    content: 'Click Me',
+    count: 0
+  };
+
+  function template() {
+    let { content, count } = state;
+    return `<button class="${className}">${content} : ${count}</button>`;
+  }
+
+  function render() {
+    let dom = document.querySelector(domQuery);
+    dom.innerHTML = template();
+  }
+
+  function updateState(obj) {
+    state = Object.assign(state, obj);
+    render();
+  }
+
+  function eventListener(evnt) {
+    updateState({ count: state.count + 1 });
+  }
+
+  function addEventListener() {
+    window.addEventListener('click', eventListener);
+  }
+
+  function mount() {
+    render();
+    addEventListener();
+  }
+
+  mount();
+
+  return { updateState };
+}
+
+let btn = Button('#btn');
